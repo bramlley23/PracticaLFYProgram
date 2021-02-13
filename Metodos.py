a006218d6393df661662fc6ca6cbd8aca3f504fb
bramlley <nefarylike@gmail.com>
2021-02-13 15:41:01 -0600

    septimo respaldo

--- Metodos.py
+++ Metodos.py
@@ -7,9 +7,20 @@ class ArchivoDEntrada:
         self.nomArchivo=nomArchivo
 
     def ordenarLista(self):
+        print("<------------------------>")
+        archi = open(self.nomArchivo)
+        lineas = archi.readlines()
+        archi.close()
+        contador = 1
+        ordenar=[]
 
-        pass
-
+        for linea in lineas:
+            if linea[0] in 'ORDENAR':
+                print(True,"numLInea=",contador)
+                ordenar.append(linea)
+            contador += 1
+        print(ordenar)
+        print("<------------------------>")
 
     def buscarEnLista(self):
         arc = open(self.nomArchivo)
@@ -18,7 +29,20 @@ class ArchivoDEntrada:
         print(linea)
 
     def ordenarBuscar(self):
-        pass
+        print("<-----------ORDENAR,BUSCAR-------------->")
+        f = open(self.nomArchivo)
+        fila=f.readlines()
+        f.close()
+        contador = 1
+        buscar=[]
+        for recta in fila:
+            if recta[2] in 'BUSCAR':
+                print(True,"numLInea=",contador)
+                buscar.append(recta)
+            contador += 1
+        print(buscar)
+
+        print("<--------------------------------------->")
 
     def mostrarListas(self):
         pass
@@ -29,4 +53,6 @@ class ArchivoDEntrada:
 
 archivo = filedialog.askopenfilename(title="Archivos", initialdir="C:/")
 abrir = ArchivoDEntrada(archivo)
-abrir.buscarEnLista()
+abrir.ordenarLista()
+#abrir.buscarEnLista()
+#abrir.ordenarBuscar()
